e197513dcb3681c9fee74184b93aae4ab50437c2
Aman Turate <amanturate@users.noreply.github.com>
2017-12-24 22:50:54 +0530

    Initial commit

---
+++ concat_array.java
@@ -0,0 +1,40 @@
+package Session_8;
+
+
+import java.util.ArrayList;
+
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+
+
+public class concat_array extends UDF{
+
+	public String evaluate (String separator, ArrayList<String> array) {
+
+		StringBuffer sBuffer;
+
+
+		if (array == null) {
+
+		return null;
+		
+		}
+
+		sBuffer = new StringBuffer();
+
+		sBuffer.append(array.get(0));
+
+		for (int i=1; i < array.size(); i++) {
+
+		sBuffer.append(separator);
+
+		sBuffer.append(array.get(i));
+
+		}
+
+
+		return sBuffer.toString();
+
+		}
+}
